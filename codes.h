fd5dd310276e3a722a7c9b63fbb44039a573724f
matefon <68391179+matefon@users.noreply.github.com>
2021-07-18 22:00:36 +0200

    Add files via upload

--- codes.h
+++ codes.h
@@ -1,101 +1,168 @@
-#include <string>
-#include <map>
+#include <string> // enable strings
+#include <map> // enable maps
 
 using namespace std;
 
-map<string,string> keycodes = { // First is the keycode, second is the TinyUSB send code
+// map of codes: first is the keycode, second is the key itself (US layout probably)
+map<string,string> keycodes = {
+    {"76","ESC"},
+    {"05","F1"},
+    {"06","F2"},
+    {"04","F3"},
+    {"0C","F4"},
+    {"03","F5"},
+    {"0B","F6"},
+    {"83","F7"},
+    {"0A","F8"},
+    {"01","F9"},
+    {"09","F10"},
+    {"78","F11"},
+    {"07","F12"},
+// printscreen and pause/break are not supported due to incompatibility with the code (pressing these keys are either interpreted as others or messes up the queue, so every read throws an error)
+    //{"E012E07C","Prt Scr"},
+    {"7E","Scroll Lock"},
+    //{"E11477E1F014E077","Pause/Break"},
+    {"0E","`"},
+    {"16","1"},
+    {"1E","2"},
+    {"26","3"},
+    {"25","4"},
+    {"2E","5"},
+    {"36","6"},
+    {"3D","7"},
+    {"3E","8"},
+    {"46","9"},
+    {"45","0"},
+    {"4E","-"},
+    {"55","="},
+    {"66","Backspace"},
+    {"0D","Tab"},
+    {"15","Q"},
+    {"1D","W"},
+    {"24","E"},
+    {"2D","R"},
+    {"2C","T"},
+    {"35","Y"},
+    {"3C","U"},
+    {"43","I"},
+    {"44","O"},
+    {"4D","P"},
+    {"54","["},
+    {"5B","]"},
+    {"5D","\\"},
+    {"58","Caps Lock"},
     {"1C","A"},
-    {"F0","release"},
-    {"E0","special"}
+    {"1B","S"},
+    {"23","D"},
+    {"2B","F"},
+    {"34","G"},
+    {"33","H"},
+    {"3B","J"},
+    {"42","K"},
+    {"4B","L"},
+    {"4C",";"},
+    {"52","'"},
+    {"5A","Enter"},
+    {"12","Shift (Left)"},
+    {"1A","Z"},
+    {"22","X"},
+    {"21","C"},
+    {"2A","V"},
+    {"32","B"},
+    {"31","N"},
+    {"3A","M"},
+    {"41",","},
+    {"49","."},
+    {"4A","/"},
+    {"59","Shift (Right)"},
+    {"14","Ctrl (left)"},
+    {"E01F","Windows (left)"},
+    {"11","Alt (left)"},
+    {"29","Spacebar"},
+    {"E011","Alt (right)"},
+    {"E027","Windows (right)"},
+    {"E02F","Menus"},
+    {"E014","Ctrl (right)"},
+    {"E070","Insert"},
+    {"E06C","Home"},
+    {"E07D","Page Up"},
+    {"E071","Delete"},
+    {"E069","End"},
+    {"E07A","Page Down"},
+    {"E075","Up Arrow"},
+    {"E06B","Left Arrow"},
+    {"E072","Down Arrow"},
+    {"E074","Right Arrow"},
+    {"77","Num Lock"},
+    {"E04A","Num /"},
+    {"7C","Num *"},
+    {"7B","Num -"},
+    {"6C","Num 7"},
+    {"75","Num 8"},
+    {"7D","Num 9"},
+    {"79","Num +"},
+    {"6B","Num 4"},
+    {"73","Num 5"},
+    {"74","Num 6"},
+    {"69","Num 1"},
+    {"72","Num 2"},
+    {"7A","Num 3"},
+    {"70","Num 0"},
+    {"71","Num ."},
+    {"E05A","Num Enter"},
+    {"F0","rel"}, // release
+    {"E0","spec"}, // special (e.g. arrow keys)
+    {"E0F0","spec_rel"} // releasing special key
 };
 
+// binary to hex converter (custom made to convert 2x4 bits separately)
 string bintohex(int arr[]){
     int sum1 = 0, sum2 = 0;
     string hex1 = "", hex2 = "";
-
     for (int i = 0; i < 8; i++){
         if (arr[i] == 1){
             switch(i){
-                case 0:
-                    sum1 += 8;
-                    break;
-                case 1:
-                    sum1 += 4;
-                    break;
-                case 2:
-                    sum1 += 2;
-                    break;
-                case 3:
-                    sum1 += 1;
-                    break;
-
-                case 4:
-                    sum2 += 8;
-                    break;
-                case 5:
-                    sum2 += 4;
-                    break;
-                case 6:
-                    sum2 += 2;
-                    break;
-                case 7:
-                    sum2 += 1;
-                    break;
+                case 0:sum1 += 8;break;
+                case 1:sum1 += 4;break;
+                case 2:sum1 += 2;break;
+                case 3:sum1 += 1;break;
+                case 4:sum2 += 8;break;
+                case 5:sum2 += 4;break;
+                case 6:sum2 += 2;break;
+                case 7:sum2 += 1;break;
             }
         }
     }
     if (sum1 > 9){
         switch(sum1){
-            case 10:
-                hex1 = "A";
-                break;
-            case 11:
-                hex1 = "B";
-                break;
-            case 12:
-                hex1 = "C";
-                break;
-            case 13:
-                hex1 = "D";
-                break;
-            case 14:
-                hex1 = "E";
-                break;
-            case 15:
-                hex1 = "F";
-                break;
+            case 10:hex1 = "A";break;
+            case 11:hex1 = "B";break;
+            case 12:hex1 = "C";break;
+            case 13:hex1 = "D";break;
+            case 14:hex1 = "E";break;
+            case 15:hex1 = "F";break;
         }
-    } else {
-        hex1 = to_string(sum1);
-    }
+    } else {hex1 = to_string(sum1);}
     if (sum2 > 9){
         switch(sum2){
-            case 10:
-                hex2 = "A";
-                break;
-            case 11:
-                hex2 = "B";
-                break;
-            case 12:
-                hex2 = "C";
-                break;
-            case 13:
-                hex2 = "D";
-                break;
-            case 14:
-                hex2 = "E";
-                break;
-            case 15:
-                hex2 = "F";
-                break;
+            case 10:hex2 = "A";break;
+            case 11:hex2 = "B";break;
+            case 12:hex2 = "C";break;
+            case 13:hex2 = "D";break;
+            case 14:hex2 = "E";break;
+            case 15:hex2 = "F";break;
         }
-    } else {
-        hex2 = to_string(sum2);
-    }
+    } else {hex2 = to_string(sum2);}
     return hex1 + hex2;
 }
 
-string getkeycode(int input[]){
-    string output = keycodes.at(bintohex(input));
+// keycode getter
+string getkey(int input[], bool spec){
+// convert input array containing ones and zeros to hex
+    string keycode = bintohex(input);
+// if special is true, then search for E0 and keycode
+    if (spec){keycode = "E0" + keycode;}
+// search for keycode in map and return its name
+    string output = keycodes.at(keycode);
     return output;
 }
--- main.cpp
+++ main.cpp
@@ -1,72 +1,86 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/timer.h"
-#include "hardware/gpio.h"
-#include "pico/util/queue.h"
+#include "hardware/timer.h" // used for repeating timer
+#include "hardware/gpio.h" // used to access gpio
+#include "pico/util/queue.h" // enable queues
 #include <queue>
-#include <string>
-#include "codes.h"
-//#include "tinyusb/device/hid.h"
+#include <string> // enable strings
+#include "codes.h" // header file containing complementary functions and the keycode map
+//#include "tinyusb/device/hid.h" <-- TODO
 
 using namespace std;
 
-const uint pin_led = 25;
-const uint pin_data = 2;
-const uint pin_clock = 3;
-const uint count_to = 10;
-uint counter = 0;
-uint led_value = 0;
-uint data_bin = 0;
-uint keypress_counter = 0;
-queue<int> dataq;
-int dataarr[8];
-string hex = "";
-bool rel = false, spec = false;
+// VARIABLES
+const uint pin_led = 25; // onboard led
+const uint pin_data = 2; // PS/2 keyboard data pin
+const uint pin_clock = 3; // PS/2 clock pin
+const uint count_to = 10; // counting to 10 clock ticks (the protocol uses 11, but somewhy it didn't work)
+uint counter = 0; // clock tick counter
+uint led_value = 0; // internal led state
+uint data_bin = 0; // converting data pin state to either 1 or 0
+queue<int> dataq; // queue for the data input
+int dataarr[8]; // array to reverse bits of the data
+string hex = ""; // used to get keycode in hex format
+string key = ""; // used to get key name
+bool rel = false, spec = false; // used to tell if a key is being released or is special (e.g. left arrow)
 
+// callback for clock pulses
 void falling_callback(uint gpio, uint32_t events){
+// while counter is less than count_to, it adds data to queue and move the counter forward
     if (counter < count_to){
         counter ++;
+// if data pin's value is not 0, the bit is 1, else 0
         if (gpio_get(pin_data) != 0){
             data_bin = 1;
         } else {
             data_bin = 0;
         }
+// adding the bit to the queue
         dataq.push(data_bin);
-        //printf("Interrupt occured at pin %d with event %d\n", gpio, events);
     } else {
-        //int len = sizeof(arr_data)/sizeof(arr_data[0]);
-        //printf("%d,\n", counter);
-        
-        // Deleting start bits (end bit is not read, but it is always 1 so who cares)
+// deleting start and parity bit (end bit is not read, but it is always 1 so who cares)
         dataq.pop();
         for (int i = 0; i < 8; i++){
             dataq.push(dataq.front());
             dataq.pop();
         }
         dataq.pop();
-
-        // Adding the queue to an array in reverse, then deleting the queue
-        //while (!dataq.empty()) {
+// adding the queue to an array in reverse, while deleting the queue
         for (int i = 7; i >= 0; i--){
             dataarr[i] = dataq.front();
             dataq.pop();
         }
+// converting the array to the hex value
         hex = bintohex(dataarr);
-        printf("HEX: %s\n", hex.c_str());
-        counter = 0;
-
-        if (hex == "F0"){rel = true;} //else {rel = false;}
-        if (hex == "E0"){spec = true;} //else {spec = false;}
-
-        if ((hex == "22") && !rel){
-            gpio_put(pin_led, true);
-        } else if ((hex == "22") && rel){
-            gpio_put(pin_led, false);
-            rel = false;
+// exception handling to prevent output for corrupted or unknown data (e.g. plugging in the keyboard) <-- TODO: detect plugging in and sleep for 200ms
+        try {
+// getting key name according to the read 8 bits
+            key = getkey(dataarr, spec);
+            if (key != "rel" && key != "spec" && key != "spec_rel"){ // if the read data means release or special, do not print
+                printf("%s ", key.c_str());
+                if (rel){printf("%s\n", "released.");} 
+                else {printf("%s\n", "pressed.");}
+            }   
+        } catch (exception& ex) {
+            printf("Data corrupted or keycode '%s' is not found.\n", hex.c_str());
         }
+// resetting clock counter
+        counter = 0;
+// turn on internal led while x is pressed (fun to watch while the rest of main functionality don't work)
+        if ((key == "X") && !rel){ // if X is pressed and not releasing, turn on internal led
+            gpio_put(pin_led, true);
+        } else if ((key == "X") && rel){ // if X is detected and is releasing, turn off internal led
+            gpio_put(pin_led, false);
+        }
+// the following 2 lines check if the release, special or both codes was sent
+// both are resetted if their values are true except some cases:
+// - release is not resetted if special is true (only when false)
+// - special is not resetted if the 'releasing special key' situation is present
+        if(hex == "F0"){rel = true;} else if(rel && spec){rel = true;} else if (rel && !spec){rel = false;}
+        if(hex == "E0"){spec = true;} else if(key == "spec_rel"){spec == true;} else if(spec){spec = false;}
     }
 }
-
+// repeating timer callback to blink internal led (now turned off so I can turn it on by pressing X)
 bool repeating_timer_callback(struct repeating_timer *t){
     led_value = 1 - led_value;
     gpio_put(pin_led, led_value);
@@ -76,16 +90,16 @@ bool repeating_timer_callback(struct repeating_timer *t){
 int main(){
     gpio_init(pin_led);
     gpio_set_dir(pin_led, GPIO_OUT);
-    
 // startup sequence: wait for first key so the queue is not false
     gpio_put(pin_led, true);
     sleep_ms(300);
     gpio_put(pin_led, false);
     sleep_ms(300);
     gpio_put(pin_led, true);
+// don't start until a key is pressed (first key's read was always corrupted, therefore the queue was not functional)
     while (gpio_get(pin_clock) != 0);
     gpio_put(pin_led, false);
-    sleep_ms(500);
+    sleep_ms(300);
     printf("Starting done.\n");
 
     stdio_init_all();
@@ -98,13 +112,26 @@ int main(){
     gpio_set_dir(pin_clock, GPIO_IN);
     gpio_pull_down(pin_clock);
 
+// repeating timer to blink internal led (now turned off so I can turn it on by pressing X)
     //struct repeating_timer timer;
     //add_repeating_timer_ms(500, repeating_timer_callback, NULL, &timer);
 
+// irq to detect clock pulses
     gpio_set_irq_enabled_with_callback(pin_clock, 0x04, 1, falling_callback);
-    //gpio_set_irq_enabled_with_callback(pin_clock, 0x08, 1, rising_callback);
 
+// main loop
     while(1){
         tight_loop_contents();
     }
 }
+
+/*-------------------------//
+// NOTES FOR MY SILLY HEAD //
+//-------------------------//
+
+line41, interrupt handler info: printf("Interrupt occured at pin %d with event %d\n", gpio, events);
+line68, key name and keycode info: printf("%s %s\n", key.c_str(), hex.c_str());
+line121, rising callback: gpio_set_irq_enabled_with_callback(pin_clock, 0x08, 1, rising_callback);
+size of an array: int len = sizeof(arr_data)/sizeof(arr_data[0]);
+
+*/
